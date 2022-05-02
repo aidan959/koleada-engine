5f0643508fdbd69e646345f6f7cb09e0512826dc
KamilaLK <03KLKrukowska@gmail.com>
2022-05-02 18:30:14 +0100

    changes to newScene

--- src/ie/engine/testing/NewScene.java
+++ src/ie/engine/testing/NewScene.java
@@ -21,30 +21,51 @@ public class NewScene  extends Scene{
         audioSync.play();
         songInfo = new SongInfo(songName);
         lastEvent = audioSync.songInfo.eventList.blank;
-
+        // bridge to chorus -- will start from bridge1
+        audioSync.song.jumpFrame((int)AudioSync.songParts.BRIDGE1.get());
     }
 
     float lerpValue;
     Animation testAnimation;
     float smoothBackground;
-    
+    float smoothCircle = 0;
+
     public void draw(){
         super.draw();
+        noStroke();
         camera();
+        lights();
         tempEvent = audioSync.isBeat();
         wasBeat = audioSync.wasBeat;
         background(0,0, smoothBackground);
+        smoothCircle = lerp(smoothCircle, (tempEvent.volume * 1000) + 5, 0.05f );        
+        // can draw stuff outside the if(wasBeat) too
         if(wasBeat){
             // runs if there was a beat provided
-            smoothBackground = lerp(0.8f,tempEvent.volume, smoothBackground );
 
-            audioSync.isBeat();
+            // only execute before / after first verse etc
+            // if(audioSync.song.positionFrame() < AudioSync.songParts.VERSE1.get() ){
+
+            // how close to next frame
+            // map(audioSync.song.positionFrame(),0, AudioSync.songParts.VERSE1.get(), 0, 100 );
+
+            // grow from 0 to 100
+            // map(audioSync.song.positionFrame(),AudioSync.songParts.BRIDGE1.get(), AudioSync.songParts.CHORUS1.get(), 0, 100 );
+
+            smoothBackground = lerp(0.8f,tempEvent.volume, smoothBackground );
+            pushMatrix();
+            translate(width/2, height/2, -50);
+            sphere(smoothCircle);
+            popMatrix();
         } else {
             
             smoothBackground = lerp(0.8f,0, smoothBackground );
-            
+            pushMatrix();
+            translate(width/2, height/2, -50);
+            sphere(smoothCircle);
+            popMatrix();
         }
-        System.out.println(bp.process());
+        // System.out.println(bp.process());
         
         // debug camera
         camera();
@@ -59,6 +80,7 @@ public class NewScene  extends Scene{
         }
     }
     public void keyPressed(){
+        super.keyPressed();
         if(key == 'n' ){
             debugger.doDebug = !debugger.doDebug;
         }
