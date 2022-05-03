8fed2ee15e1aaf70ffb132556dab028b2891e9d8
myusername <myemail>
2022-05-04 00:23:03 +0100

    gradient circles with map, need to get circles more responsive - why only half beats

--- src/ie/engine/testing/TweedyNS.java
+++ src/ie/engine/testing/TweedyNS.java
@@ -29,10 +29,12 @@ public class TweedyNS extends Scene{
     Animation testAnimation;
     float smoothBackground;
     float smoothCircle = 0;
+    float smoothCircle2 = 0;
     float circleFill = 0;
     float angle = 0;
     int filled = 0;
     int maxCubes = 4;
+    int circles = 1;
     
     public float border = 0;
 
@@ -45,30 +47,13 @@ public class TweedyNS extends Scene{
         wasBeat = audioSync.wasBeat;
         background(0,0, smoothBackground);
         smoothCircle = lerp(smoothCircle, (tempEvent.volume * 1000) + 5, 0.07f);
+        smoothCircle2 = lerp(smoothCircle, (tempEvent.volume * 555) + 5, 0.07f);
         int currentFrame = audioSync.song.positionFrame();
+        
 
         // rotateX(sin(angle));
         // rotateY(angle);
-
-        if(currentFrame > 9907884) {
-            // cuboids
-            for (int i = 1; i < maxCubes; i++) {
-                for (int j = 1; j < maxCubes; j++) {
-                    pushMatrix();
-                    // translate(width*i/4, height*j/4, map(sin(audioSync.song.positionFrame()), 0, 100000, 60, 50));
-                    rectMode(CENTER);
-                    stroke(0, 255, 140);
-                    noFill();
-                    translate(width*i/4, height*j/4, map(sin(angle), -1, 1, -1000, -100));
-                    rotate(sin(angle)*2);
-                    rotateY(angle);
-                    rotateZ(sin(angle));
-                    box(70);
-                    popMatrix();
-                }
-            }
-        }
-
+        
         // can draw stuff outside the if(wasBeat) too
         if (wasBeat) {
             // runs if there was a beat provided
@@ -83,6 +68,11 @@ public class TweedyNS extends Scene{
             // map(audioSync.song.positionFrame(),AudioSync.songParts.BRIDGE1.get(), AudioSync.songParts.CHORUS1.get(), 0, 100 );
 
             smoothBackground = lerp(0.8f,tempEvent.volume, smoothBackground );
+            if (circles > 9) {
+                circles = 1;
+            } else {
+                circles++;
+            }
             // sphere grid
             noStroke();
             for (int i = 0; i < 9; i+=1) {
@@ -95,36 +85,16 @@ public class TweedyNS extends Scene{
                     }
                     float mapX = map(i, 0, 9, 0, width);
                     float mapY = map(j, 0, 9, 0, height);
-                    // circle(mapX+20, mapY+20, 40);
+                    circle(mapX+20, mapY+20, 40);
                     
                     pushMatrix();
-                    translate(mapX+25, mapY+25, -10);
-                    sphere(smoothCircle);
+                    translate(mapX+25, mapY+25, -5);
+                    sphere(smoothCircle+5);
                     popMatrix();
                 }
             }
 
-            // cuboids
-                for (int i = 1; i < 4; i++) {
-                    for (int j = 1; j < 4; j++) {
-                        pushMatrix();
-                        // translate(width*i/4, height*j/4, map(sin(audioSync.song.positionFrame()), 0, 100000, 60, 50));
-                        rectMode(CENTER);
-                        // stroke(0, 255, 140);
-                        stroke(255, 0, 0);
-                        noFill();
-                        fill(random(100, 240), random(100, 240), random(100, 240));
-                        translate(width*i/4, height*j/4, map(sin(angle), -1, 1, -1000, -100));
-                        rotate(sin(angle)*2);
-                        rotateY(angle*5100);
-                        rotateZ(sin(angle));
-                        scale(10);
-                        box(70);
-                        popMatrix();
-                    }
-                }
-
-            System.out.println(audioSync.song.positionFrame());
+            // System.out.println(audioSync.song.positionFrame());
         } else {
             smoothBackground = lerp(0.8f,0, smoothBackground );
             
@@ -134,35 +104,66 @@ public class TweedyNS extends Scene{
             // to the range (var4->var5) for fill() etc
             fill(map(sin(angle), -1, 1, 0, 255), map(sin(angle*5), -1, 1, 0, 255), map(sin(angle), -1, 1, 0, 255));
 
-            // center sphere
+
+            // center circle
             pushMatrix();
-            // fill(255, 0, 119);
-            translate(width/2, height/2, -100);
-            // sphere(smoothCircle);
-            circle(width/2-100, height/2-100, 200);
+            noFill();
+            strokeWeight(5);
+            for (int i = 0; i < circles; i++) {
+                if (currentFrame < 9907784){
+                    stroke(map(sin(angle*4), -1, 1, 125, 255)+(i*10), 0, 199);
+                } else {
+                    stroke(0, map(sin(angle*4), -1, 1, 150, 255)+(i*10), 199);
+                    // stroke(0, 255, 119);
+                }
+                circle(width/2, height/2, (50+(i*50))+smoothCircle2*2);
+            }
+            // circle(width/2, height/2, smoothCircle*10);
             popMatrix();
+
             
             noStroke();
             for (int i = 0; i < 9; i+=1) {
                 for (int j = 0; j < 9; j+=1) {
+                    fill(0, 255, 128);
                     // float c = map(arg0, arg1, arg2, arg3, arg4)
-                    // float c = map((i * j), 0, 9, 0, 255) % 256;
-                    if ((frameCount % 120) == 0) {
-                        fill(random(100, 240), random(100, 240), random(100, 240));
+                    float c = map((i * j), 0, 9, 0, 255) % 256;
+                    if (currentFrame < 9907784){
+                        fill(c+180, c+12, c+60);
+                    } else {
+                        fill(c+12, c+100, c+60);
                     }
-                    // fill(c, c+12, c+60);
                     float mapX = map(i, 0, 9, 0, width);
                     float mapY = map(j, 0, 9, 0, height);
-                    // circle(mapX+20, mapY+20, 40);
                     
                     pushMatrix();
-                    translate(mapX+25, mapY+25, -10);
-                    sphere(smoothCircle);
+                    translate(mapX+25, mapY+25, -5);
+                    sphere(smoothCircle+5);
                     popMatrix();
                 }
             }
             
             
+            
+        }
+        if(currentFrame > 9907784 ) {
+            // cuboids
+            for (int i = 1; i < maxCubes; i++) {
+                for (int j = 1; j < maxCubes; j++) {
+                    strokeWeight(1);
+                    pushMatrix();
+                    // translate(width*i/4, height*j/4, map(sin(audioSync.song.positionFrame()), 0, 100000, 60, 50));
+                    rectMode(CENTER);
+                    stroke(0, 255, 140);
+                    noFill();
+                    translate(width*i/4, height*j/4, map(sin(angle), -1, 1, -1000, -100));
+                    rotate(sin(angle)*2);
+                    rotateY(angle);
+                    rotateZ(sin(angle));
+                    box(70);
+                    popMatrix();
+                }
+            }
         }
         // System.out.println(bp.process());
         
